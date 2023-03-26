cea387e8581530b22733eea08527e79b8763a269
AndresD2314 <lemon108andres@gmail.com>
2023-03-26 09:35:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/parcial1/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.parcial1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText : EditText= findViewById(R.id.EditText)
+        val btnLista : Button = findViewById(R.id.btnLista)
+
+        btnLista.setOnClickListener {
+            val numero = Integer.parseInt(editText.text.toString())
+
+            if (numero < 0 || numero > 100) {
+                Toast.makeText(this, "El numero no se encuentra dentro del rango!", Toast.LENGTH_SHORT).show()
+                Log.i("RANGO", "El numero no esta en el rango!")
+            } else if (esPrimo(numero)) {
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("numero", numero)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "El numero no es primo!", Toast.LENGTH_SHORT).show()
+                Log.i("NUMERO PRIMO", "El numero no es primo!")
+            }
+
+        }
+    }
+
+    private fun esPrimo (numero : Int) : Boolean {
+        for (i in 2 .. numero) {
+            if (numero % i == 0)
+                return false
+        }
+        return true
+    }
+
+
+}
---
+++ app/src/main/java/com/example/parcial1/MainActivity2.kt
@@ -0,0 +1,33 @@
+package com.example.parcial1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity2 : AppCompatActivity() {
+
+    private val lvDatos : ListView = findViewById(R.id.LvNumeros)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val arrayAdapter: ArrayAdapter<*>
+        val arr = mutableListOf<Int>()
+
+
+        val numero = intent.getIntExtra("numero", 0)
+
+        var x = numero
+        var cuadrado = 0
+        while (cuadrado < 10000) {
+            cuadrado = x * x
+            arr.add(cuadrado)
+            x++
+        }
+
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arr)
+        lvDatos.adapter = arrayAdapter
+    }
+}
